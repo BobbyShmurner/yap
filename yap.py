c8aa9f417957407ec75ee2abe0bd2d13e053242d
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2022-11-07 22:41:39 +0000

    Partial commit for tags

--- yap.py
+++ yap.py
@@ -1,4 +1,16 @@
+import requests
+
 from setuptools._vendor.packaging import tags, version
 
 def install(package: str, version: str = "*"):
     print(f"Installing Package \"{package.title()}\"...")
+
+    response = requests.get(f"https://pypi.org/pypi/{package}/json")
+    content = str(response.content)
+
+    for tag in tags.sys_tags():
+        if str(tag) in content:
+            print(f"Found compatible wheel \"{tag}\"!")
+            break
+        else:
+            print(f"No wheel for tag \"{tag}\"")
